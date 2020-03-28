5f45e1fdc5a1c75e932a17ad2d66a9707748a8e5
nas <nas.silver.bullet@gmail.com>
2020-03-28 20:25:31 +0900

    :+1: Add sounds

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,15 @@
-const keyA = document.querySelector('.keyA');
+function playSound(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  if (!audio) return;
+  const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+
+  key.classList.add('playing');
+  audio.currentTime = 0;
+  audio.play();
+  setTimeout(() => {
+    key.classList.remove('playing');
+  }, 100);
+}
+
+const keys = Array.from(document.querySelectorAll('.key'));
+window.addEventListener('keydown', playSound);
