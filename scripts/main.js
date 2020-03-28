1ff4c9112eb838622a7b62c122cf0b7ad9428ca4
nas <nas.silver.bullet@gmail.com>
2020-03-28 21:41:35 +0900

    :+1: Enable to click

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,24 @@
-function playSound(e) {
-  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+const playSound = code => {
+  const audio = document.querySelector(`audio[data-key="${code}"]`);
   if (!audio) return;
-  const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`kbd[data-key="${code}"]`);
 
-  key.classList.add('playing');
+  key.classList.add("playing");
   audio.currentTime = 0;
   audio.play();
   setTimeout(() => {
-    key.classList.remove('playing');
+    key.classList.remove("playing");
   }, 100);
-}
+};
 
-const keys = Array.from(document.querySelectorAll('.key'));
-window.addEventListener('keydown', playSound);
+const playSoundOnClicked = e => {
+  playSound(e.target.dataset.key);
+};
+
+const playSoundOnTapped = e => {
+  playSound(e.keyCode);
+};
+
+const keys = Array.from(document.querySelectorAll(".key"));
+keys.forEach(e => e.addEventListener("click", playSoundOnClicked));
+window.addEventListener("keydown", playSoundOnTapped);
